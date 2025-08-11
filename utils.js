99472e1a2e7d3a9529f64ccd72e31da7ae1050dc
Voke <google@voke.com>
2025-08-11 13:00:32 +0800

    feat: todo in vanilla

--- utils.js
+++ utils.js
@@ -6,3 +6,9 @@ export const getTodos = () => {
 export const saveTodos = (todos) => {
   localStorage.setItem('todos', JSON.stringify(todos));
 }
+
+export function uuid() {
+  const ts = Date.now().toString(36);
+  const rand = Math.random().toString(36).substring(2, 8);
+  return ts + rand;
+}
